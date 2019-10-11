20523afb63608a0b42b9254b8a9c051829677e04
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-10-11 19:30:49 +0800

    Initial commit

---
+++ lib/goals/goals_screen.dart
@@ -0,0 +1,26 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class GoalsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GoalsScreenState();
+  }
+}
+
+class GoalsScreenState extends State<GoalsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Goals'),
+      ),
+      body: Center(
+        child: Text('Helo world'),
+      ),
+    );
+  }
+}
